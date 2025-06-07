02471cc09be1e457979f9e7688add6095805f9b2
Vidit Maheshwari <22mc3037@rgipt.ac.in>
2025-06-07 19:02:25 +0530

    fix: adding login methods and wallets

--- src/components/PrivyProvider.tsx
+++ src/components/PrivyProvider.tsx
@@ -1,19 +1,24 @@
 'use client';
 
 import {PrivyProvider} from '@privy-io/react-auth';
+import { monadTestnet } from "viem/chains";
 
 export default function Providers({children}: {children: React.ReactNode}) {
   return (
     <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_APP_ID!}
-      config={{
-        // Create embedded wallets for users who don't have a wallet
+    appId={process.env.NEXT_PUBLIC_APP_ID!}
+    config={{
+        appearance: {
+            theme: "light",
+            walletChainType: "ethereum-only",
+        },
+        defaultChain: monadTestnet,
+        supportedChains: [monadTestnet],
+        loginMethods: ["google", "passkey", "wallet"],
         embeddedWallets: {
-          ethereum: {
-            createOnLogin: 'all-users'
-          }
-        }
-      }}
+            ethereum: { createOnLogin: "all-users" },
+        },
+    }}
     >
       {children}
     </PrivyProvider>
